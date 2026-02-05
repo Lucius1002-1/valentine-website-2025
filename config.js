52313a74d21b22734a2db8c9fe1893624fb6e768
Lucius1002-1 <sammyphan49@gmail.com>
2026-02-05 17:49:49 -0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine Lluvia? 💝",
+    pageTitle: "Hi Lluvia, Guess What?",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dwfdklsux/video/upload/v1770330825/Amor_eid6yx.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dwfdklsux/video/upload/v1770331768/amor_2_buisyj.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
